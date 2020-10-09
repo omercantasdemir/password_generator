75baacf07c69c51fab32f2def4b8a951f4afa401
Ömercan Taşdemir <omercantasdemir@hotmail.com>
2020-10-09 14:20:43 +0300

    update code
    Added some command lines because I forgot the first time.

--- password-gen.cpp
+++ password-gen.cpp
@@ -13,26 +13,22 @@ int main()
     std::cout<<("Please state how many digits you want in your password: "); std::cin>>(cnt);
     val=randomize(cnt);
     for (size_t i = 0; i < cnt; i++)
-    {
-        std::cout<<val[i];
-    }
+    std::cout<<val[i];//Password printed out
     std::cout<<std::endl;
     std::cout<<"Would you like to save this password to a .txt file?(Y/N)";
     if(check())
-    {
-        reg(val);
-    }
+    reg(val);
     std::cout<<"Would you like to create another password?(Y/N)";
     }while (check());
 }
 char *randomize(int input)
 {
     srand(time(0));
-    d=new char[input];
+    d=new char[input];//Dynamic array creation
     for (size_t i = 0; i < input; i++)
     {
         int r;
-        r=rand()%74+48;//48-122-(58-64)&&-(91-96)       
+        r=rand()%74+48;//48-122-(58-64)&&-(91-96)
         for (size_t j = 58; j < 65; j++)//Checking if char is as wanted
         {if (r==j) r=48+rand()%9;}
         for(size_t k=91;k<97;k++)//Checking if char is as wanted
@@ -53,6 +49,7 @@ bool check()
 }
 void reg(char *input)
 {
+    //File stream
     std::string name;
     std::ofstream save;
     save.open("totally not passwords.txt", std::ios::app);
